932d307efbfa8b7d4b230eeed73e6adaf9474afc
taufikfadjar <taufikfadjar@live.com>
2017-08-10 13:54:01 +0700

    Update example

---
+++ MirokuExample/MirokuExample.CodeFirstDB/CodingFirstContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MirokuExample.CodeFirstDB
+{
+    [Table("EdmCategories", Schema = "DBA")]
+    public class Category
+    {
+        public string CategoryID { get; set; }
+        [MaxLength(64)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+
+    [Table("EdmProducts", Schema = "DBA")]
+    public class Product
+    {
+        public int ProductId { get; set; }
+        [MaxLength(64)]
+        public string Name { get; set; }
+        public string CategoryID { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+
+    [Table("EdmSuppliers", Schema = "DBA")]
+    public class Supplier
+    {
+        [Key]
+        public string SupplierCode { get; set; }
+        [MaxLength(64)]
+        public string Name { get; set; }
+    }
+
+    public class CodingFirstContext : DbContext
+    {
+        public CodingFirstContext() : base() { }
+        public CodingFirstContext(string connStr) : base(connStr) { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Supplier>().Property(s => s.Name).IsRequired();
+        }
+    }
+}
--- MirokuExample/MirokuExample/Program.cs
+++ MirokuExample/MirokuExample/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MirokuExample.CodeFirstDB;
+using Sap.Data.SQLAnywhere;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,15 @@ namespace MirokuExample
     {
         static void Main(string[] args)
         {
+            Database.DefaultConnectionFactory = new SAConnectionFactory();
+            Database.SetInitializer<CodingFirstContext>(
+                new DropCreateDatabaseAlways<CodingFirstContext>());
+
+            using (var db = new CodingFirstContext(
+                "DSN=Miroku"))
+            {
+                var query = db.Products.ToList();
+            }
         }
     }
 }
